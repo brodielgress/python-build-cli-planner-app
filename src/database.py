a99a449e53cbb0dcc4e8358ca5941960b5c5a5fc
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-10-23 20:21:02 -0400

    Done with interactive lessons!

--- src/database.py
+++ src/database.py
@@ -13,13 +13,13 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date, ReminderClass):    
+def add_reminder(text, date, ReminderClass):
     reminder = ReminderClass(text, date)
 
     if not isinstance(reminder, DeadlinedReminder):
-        TypeError('Invalid Reminder Class')
+        raise TypeError('Invalid Reminder Class')
 
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow(reminder)
+        writer.writerow([reminder])
